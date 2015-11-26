1348b6c2ad6b39e0caf0460f076e63a20d52ad19
Rick Stallkamp <rickstallkamp@gmail.com>
2015-11-26 14:47:58 -0800

    Updated file to now read in files and parse input. Also included a test file called cfg.txt

--- cky.py
+++ cky.py
@@ -1,8 +1,38 @@
 #!/usr/local/bin/python3 
 import sys
 
-def read_line():
-    for i in sys.stdin:
-        print(i)
-   
-read_line()   
+def parse_line():
+    grammarList = []
+    for line in sys.stdin:
+        if len(line) <= 1:
+            break
+        line = line.strip('\n') 
+        rule = line.split('->')
+        lhs = rule[0].strip(' ')
+        rhsSides = rule[1].split('|')
+        
+        
+        for rhs in rhsSides:
+            rhs = rhs.strip(' ').split(' ')
+            rule = {'lhs': lhs}
+            if len(rhs) == 1:
+                rule['Term'] = rhs[0]
+            else:
+                rule['Prod'] = rhs
+
+         
+        grammarList.append(rule)
+    return grammarList
+
+
+def parse_sentences():
+    sentences = []
+    for line in sys.stdin:
+        sentences.append(line.strip('\n').split(' '))
+    return sentences
+
+
+#init_p_matrix():
+    
+    
+print(parse_line(), parse_sentences())
